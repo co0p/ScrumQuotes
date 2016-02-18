d2b7068cc0cdf2df060eb004967e041319a0d29a
Julian Godesa <julian.godesa@asideas.de>
2016-02-18 10:55:01 +0100

    added cors

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # ScrumQuotes
+
+This is a simple restlike service returning quotes and an agile interpretation. You can see a live demo at:
+
+
+## get all quotes
+ 
+/quotes
+
+
+## get one quote
+ 
+/quotes/:id
+
+
+## get a random quote
+ 
+/quotes/random
+
+
--- server.js
+++ server.js
@@ -2,9 +2,18 @@ var express = require('express');
 
 var app = express();
 var port = 5432;
-
 var quotes = require('./quotes.json');
 
+/* CORS middleware */
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+app.use(allowCrossDomain);
+
+/* the endpoints */
 app.get('/', function(req, res) {
   res.type('text/plain');
   res.send('Quotes server serving ', quotes.length, ' quotes');
@@ -26,9 +35,6 @@ app.get('/quotes/:id', function(req, res) {
   res.json(q);
 });
 
-app.get('/quotes/random', function(req, res) {
-
-});
-
+/* start the server */
 app.listen(process.env.PORT || port);
 console.log('listening on port', process.env.PORT || port);
